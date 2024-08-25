b99641dca5592331d4da66bd47e29ea15fdfa54f
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-08-25 19:23:39 -0300

    added js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let $ = (element) => document.querySelector(element)
+let $$ = (elements) => document.querySelectorAll(elements)
+
+$('.header__title__container').addEventListener('click', () => {
+	console.log('hola')
+})
