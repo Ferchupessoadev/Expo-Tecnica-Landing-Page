5d01368c9dc96f10364bb21016339453c74788ba
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-08-25 22:10:24 -0300

    added header and navs. No responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-let $ = (element) => document.querySelector(element)
-let $$ = (elements) => document.querySelectorAll(elements)
-
-$('.header__title__container').addEventListener('click', () => {
-	console.log('hola')
-})
+const $ = selector => {
+	return document.querySelector(selector);
+}
+const $$ = selector => {
+	return document.querySelectorAll(selector);
+}
